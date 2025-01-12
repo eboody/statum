a663ce360ed503de92ea0467ca73e1eab694d566
Eran Boodnero <eboodnero@gmail.com>
2025-01-11 16:22:31 -0800

    feat(core): implement Display and Error traits for Error enum

--- statum-core/src/lib.rs
+++ statum-core/src/lib.rs
@@ -2,3 +2,11 @@
 pub enum Error {
     InvalidState,
 }
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
+        write!(fmt, "{self:?}")
+    }
+}
+
+impl std::error::Error for Error {}
