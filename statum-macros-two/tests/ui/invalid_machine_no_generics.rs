7d8c93156f7319412a030c4ff820c05a6412950f
Eran Boodnero <eboodnero@gmail.com>
2025-02-13 13:53:19 -0800

    LETS FUCKING GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- statum-macros-two/tests/ui/invalid_machine_no_generics.rs
+++
@@ -1,8 +0,0 @@
-extern crate statum_macros_two as statum_macros;
-use statum_macros::machine;
-
-#[machine]
-struct Machine {
-    // ❌ Missing `<S: State>`
-    client: String,
-}
--- statum-macros-two/tests/ui/invalid_state_struct.rs
+++
@@ -1,7 +0,0 @@
-extern crate statum_macros_two as statum_macros;
-use statum_macros::state;
-
-#[state]
-struct NotAnEnum {
-    value: u32,
-}
